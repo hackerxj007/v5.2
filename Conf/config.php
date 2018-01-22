177e19cfbda4ef12650b6e4bdbc769453cca5219
sailor1024 <1663852293@qq.com>
2018-01-22 20:48:21 +0800

    1

--- Conf/config.php
+++ Conf/config.php
@@ -12,6 +12,6 @@ return array(
     'DB_HOST'               => '127.0.0.1', // 服务器地址
     'DB_NAME'               => 'v5',          // 数据库名
     'DB_USER'               => 'root',      // 用户名
-    'DB_PWD'                => '',          // 密码
+    'DB_PWD'                => '123456',          // 密码
      'DB_PREFIX'             => 'index_',    // 数据库表前缀	
 );
